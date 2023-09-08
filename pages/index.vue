ff3d49acbffddc843ebb86e4c6ba1586cca0e150
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-09-08 21:25:06 +0200

    intial data from single board

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,32 @@
 <template>
-  <TheAlert
-    ref="alertComponent"
-    text="You have successfully logged in"
-    color="#006200"
-  />
+  <div>
+    <TheAlert
+      ref="alertComponent"
+      text="You have successfully logged in"
+      color="#006200"
+    />
+    {{ singleBoard }}
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import TheAlert from "~/components/UI/TheAlert.vue";
 export default {
   name: "IndexPage",
   components: { TheAlert },
+  computed: {
+    ...mapGetters("board", ["singleBoard"]),
+  },
   watch: {
     "$auth.loggedIn"(loggedIn) {
       if (loggedIn) {
         this.$refs.alertComponent.show();
       }
     },
+  },
+  async mounted() {
+    await this.$store.dispatch('board/fetchBoards')
   },
 };
 </script>
